8973f0dfbf7b5587cf03e1e5e3c54fd2a9de38f8
Tiago Fernandez <tiago.fernandez@gmail.com>
2011-02-10 16:29:27 +0100

    Reduce noise.

--- scripts/gitstatus.rb
+++ scripts/gitstatus.rb
@@ -17,17 +17,16 @@ end
 
 def check_uncommitted_and_unpushed
   status = `git status 2>&1 && git log origin/master..HEAD`
-  not_gitrepo = (status =~ /fatal/)
-  all_committed = (status =~ /nothing to commit/i)
-  branch_ahead = (status =~ /Your branch is ahead/)
-  if not_gitrepo
-    print "#{@bcolors[:ok_blue]}not git repository.#{@bcolors[:endc]}\n"
-  elsif not all_committed
-    print "#{@bcolors[:warning]}uncommitted changes.#{@bcolors[:endc]}\n"
-  elsif branch_ahead
-    print "#{@bcolors[:warning]}unpushed commits.#{@bcolors[:endc]}\n"
+  if (status =~ /fatal/)
+    print "not git repository."
   else
-    print "#{@bcolors[:ok_green]}in sync.#{@bcolors[:endc]}\n"
+    if not (status =~ /nothing to commit/i)
+      print "#{@bcolors[:warning]}uncommitted changes.#{@bcolors[:endc]}\n"
+    elsif (status =~ /Your branch is ahead/)
+      print "#{@bcolors[:warning]}unpushed commits.#{@bcolors[:endc]}\n"
+    else
+      print "#{@bcolors[:ok_green]}in sync.#{@bcolors[:endc]}\n"
+    end
   end
 end
 
