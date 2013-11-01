7ec724819870f6c4ddede2b3a9faffa8790178ea
Tiago Fernandez <tiago.fernandez@gmail.com>
2013-11-01 13:26:58 +0100

    Add missing definitions from Bash profile

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,3 +1,25 @@
+# http://ridiculousfish.com/shell/user_doc/html/
+
+# Change the greeting message
+set fish_greeting 'Unleash your awesomeness!'
+
+# Set Sublime Text as default editor
+set -gx EDITOR subl
+set -gx VISUAL subl
+
+# Define bin path variables
+set postgresql /Library/PostgreSQL/9.3/bin
+set mongodb $HOME/Toolbox/mongodb-osx-x86_64-2.4.7/bin
+set heroku /usr/local/heroku/bin
+set groovy /usr/local/Cellar/groovy/2.1.8/bin
+set npm /usr/local/share/npm/bin
+set pyenv $HOME/.pyenv/versions/2.7.5/bin $HOME/.pyenv/shims
+set rvm $HOME/.rvm/bin
+
+# Define the path
+set -gx PATH $rvm $pyenv $npm $groovy $heroku $mongodb $postgresql $PATH
+
+# Configure Git's prompt
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showupstream 'yes'
@@ -7,7 +29,8 @@ set __fish_git_prompt_char_stagedstate '→'
 set __fish_git_prompt_char_stashstate '↩'
 set __fish_git_prompt_char_upstream_ahead '↑'
 set __fish_git_prompt_char_upstream_behind '↓'
- 
+
+# Customize the default prompt
 function fish_prompt
 	set last_status $status
 	set_color $fish_color_cwd
@@ -16,3 +39,15 @@ function fish_prompt
 	printf '%s ' (__fish_git_prompt)
 	set_color normal
 end
+
+# Utility to reload the configuration
+function reload_fish
+  . $HOME/.config/fish/config.fish
+  echo "Fish configuration reloaded."
+end
+
+# Initialize Pyenv
+. (pyenv init - | psub)
+
+# Show hidden files by default
+alias ls='ls -a'
