be0c623d022c4aee502bd966f751f26902811dbf
Tiago Fernandez <tiago.fernandez@gmail.com>
2020-05-23 10:27:54 -0400

    Remove stuff I don't use anymore

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -12,14 +12,8 @@ set fish_greeting ''
 set -gx EDITOR vim
 set -gx VISUAL vim
 
-# Docker
-alias docker_rm_all='docker rm (docker ps -a -q)'
-alias docker_rm_clean='docker ps -a | egrep "weeks ago|months ago" | awk "{print $1}" | xargs --no-run-if-empty docker rm'
-alias docker_rmi_all='docker rmi (docker images -q)'
-alias docker_rmi_clean='docker images | awk "/^<none>/ {print $3}" | xargs docker rmi'
-
 # Java
-set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/openjdk-12.0.1.jdk/Contents/Home/
+set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/openjdk-14.0.1.jdk/Contents/Home/
 
 # NVM
 set -gx NVM_DIR $HOME/.nvm
@@ -30,17 +24,6 @@ end
 
 # Python
 pyenv init - | source
-
-function pip_uninstall_all --description='Uninstall all Python packages.'
-  pip freeze | grep -v "^-e" | xargs pip uninstall -y
-end
-
-# Ruby
-function gem_uninstall_all --description='Uninstalls all Ruby gems.'
-  for each in (gem list --no-version)
-    sudo gem uninstall -aIx $each
-  end
-end
 
 # Zlib
 set -gx LDFLAGS "$LDFLAGS -L/usr/local/opt/zlib/lib"
@@ -69,43 +52,15 @@ function source_env
   end
 end
 
-# Searches for class in file system of jars
-function search_class
-  find -name \*.jar | xargs -n1 -iFILE sh -c "jar tvf FILE | sed -e s#^#FILE:#g" | grep $argv\\.class | cut -f1 -d:
-end
-
-# Displays the internal IP
-function ip_internal
-  ifconfig | grep -v 127.0.0.1 | sed -En 's/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'
-end
-
-# Displays the external IP
-function ip_external
-  curl icanhazip.com
-end
-
-# Creates a directory and enters it
-function mkcd
-  mkdir -p $argv
-  cd $argv
-end
-
 # Renames JPEG files according to their creation dates
 function rename_jpg
-  bash -c 'for i in *.JPG; do newname=$(stat -t %Y%m%d%H%M%S -f %Sm -- $i); mv -- $i $newname.jpg; done'
+  bash -c 'for i in *.jpg; do newname=$(stat -t %Y%m%d%H%M%S -f %Sm -- $i); mv -- $i $newname.jpg; done'
 end
 
 # Show hidden files by default
 alias ls='ls -a'
 alias ll='ls -la'
 
-# Set useful variables
-set -gx INTERNAL_IP (ip_internal)
-
-# Unleash the party parrot!
-alias parrot='curl http://parrot.live'
-
-# Run last command as root
 # Run last command as root
 alias please='eval command sudo $history[1]'
 
